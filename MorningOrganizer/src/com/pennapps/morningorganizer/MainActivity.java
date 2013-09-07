96acbb151432a836c7f0a494c851f3277da501ba
apenugonda <apenugon@andrew.cmu.edu>
2013-09-07 14:12:37 -0400

    BOOM

--- MorningOrganizer/src/com/pennapps/morningorganizer/MainActivity.java
+++ MorningOrganizer/src/com/pennapps/morningorganizer/MainActivity.java
@@ -12,13 +12,15 @@ import android.content.IntentFilter;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.view.Menu;
 import android.view.View;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
 
-public class MainActivity extends Activity implements View.OnClickListener {
+public class MainActivity extends FragmentActivity implements View.OnClickListener {
 	//Time length of one second
 	final static private long SECOND = 1000;
 	
@@ -32,6 +34,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
 	
 	
 	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -39,7 +42,6 @@ public class MainActivity extends Activity implements View.OnClickListener {
 		nuanceObject = new Nuance();
 		//Assign listener to the button
 		findViewById(R.id.startButton).setOnClickListener(this);
-		
 		setup();
 	}
 	
@@ -69,6 +71,33 @@ public class MainActivity extends Activity implements View.OnClickListener {
 		alarmManager = (AlarmManager)(this.getSystemService(Context.ALARM_SERVICE));
 	}
 
+	public void onClickTwo(View v) {
+		Context c = v.getContext().getApplicationContext();
+		FragmentManager fm = getSupportFragmentManager();
+
+		ScheduleDialog dialog = new ScheduleDialog();
+		/*dialog.setOnDialogResultListener(new ScheduleDialog.OnDialogResultListener() {
+			
+			@Override
+			public void onReturn(int[] returnval) {
+				Context c = getApplicationContext();
+				doStuff(c);
+			}
+		});*/
+		dialog.show(fm, "scheduler");
+		//Toast.makeText(c, "Scheduled Alarm!", Toast.LENGTH_LONG).show();
+	}
+	
+	public void onUserSelectValue(int[] returnVal)
+	{
+		Context c = getApplicationContext();
+		Toast.makeText(c, "Scheduled Alarm2!", Toast.LENGTH_LONG).show();
+		alarmTimeCal.set(Calendar.HOUR_OF_DAY, returnVal[0]);
+		alarmTimeCal.set(Calendar.MINUTE, returnVal[1]);
+		alarmTimeCal.set(Calendar.SECOND, 0);
+		alarmManager.set(AlarmManager.RTC_WAKEUP, alarmTimeCal.getTimeInMillis(), pendingIntent);
+	}
+	
 	@Override
 	public void onClick(View v) {
 		//Do stuff right now
@@ -83,7 +112,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
 		alarmTimeCal.set(Calendar.SECOND, 0);
 		*/
 
-		Toast.makeText(c, "Announcing...", Toast.LENGTH_LONG).show();
+		Toast.makeText(c, "Preparing...", Toast.LENGTH_LONG).show();
 		doStuff(c);
 		//Start the alarm manager to wake up the app
 		//alarmManager.set(AlarmManager.RTC_WAKEUP, alarmTimeCal.getTimeInMillis(), pendingIntent);
@@ -95,6 +124,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+
 	
 	@Override
 	protected void onDestroy()
