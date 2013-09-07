ffd23fe4f517ea53c6d00b3167cc623584cc5def
apenugonda <apenugon@andrew.cmu.edu>
2013-09-07 14:51:06 -0400

    Added stuff

--- MorningOrganizer/src/com/pennapps/morningorganizer/ScheduleDialog.java
+++ MorningOrganizer/src/com/pennapps/morningorganizer/ScheduleDialog.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TimePicker;
 
 public class ScheduleDialog extends DialogFragment {
