8d22e62eab06b0e07b95352dfa0682be9efb61d1
hemanthkini <hemanth.kini@gmail.com>
2013-09-07 02:58:01 -0400

    Create NuanceAppInfo.java

---
+++ MorningOrganizer/src/com/pennapps/morningorganizer/NuanceAppInfo.java
@@ -0,0 +1,21 @@
+package com.pennapps.morningorganizer;
+
+/* lololol */
+public class NuanceAppInfo {
+
+	public static final String SpeechKitServer = "sandbox.nmdp.nuancemobility.net"/* Enter your server here */;
+
+    public static final int SpeechKitPort = 443/* Enter your port here */;
+    
+    public static final boolean SpeechKitSsl = false;
+
+    public static final String SpeechKitAppId = "NMDPTRIAL_hemanthkini20130907031341" /* Enter your ID here */;
+
+    public static final byte[] SpeechKitApplicationKey = {
+        /* Enter your application key here:
+        (byte)0x00, (byte)0x01, ... (byte)0x00
+        */
+    	(byte)0xa1, (byte)0x48, (byte)0x9a, (byte)0x42, (byte)0x86, (byte)0x74, (byte)0xd0, (byte)0x01, (byte)0xd2, (byte)0x62, (byte)0x0d, (byte)0xff, (byte)0x19, (byte)0x2e, (byte)0x88, (byte)0xbc, (byte)0x74, (byte)0x3f, (byte)0x04, (byte)0xe6, (byte)0xcd, (byte)0xde, (byte)0x95, (byte)0x19, (byte)0x43, (byte)0x2e, (byte)0x34, (byte)0x25, (byte)0x76, (byte)0x4f, (byte)0x62, (byte)0x17, (byte)0x97, (byte)0xfc, (byte)0x5a, (byte)0x1e, (byte)0x70, (byte)0x2a, (byte)0x1f, (byte)0x43, (byte)0x8c, (byte)0x2c, (byte)0x42, (byte)0xa6, (byte)0x5b, (byte)0xcb, (byte)0x62, (byte)0xcf, (byte)0x0a, (byte)0x66, (byte)0xb5, (byte)0xd8, (byte)0x44, (byte)0x4b, (byte)0xf1, (byte)0x87, (byte)0x79, (byte)0x26, (byte)0xac, (byte)0xef, (byte)0x75, (byte)0x0f, (byte)0x07, (byte)0x0d
+    };
+	
+}
