18c2c534f298381933d877f6a9fef88f71f47336
Hemanth Kini <hemanth.kini@gmail.com>
2013-09-07 19:12:52 -0400

    funny social names

--- MorningOrganizer/src/com/pennapps/morningorganizer/GetInfoTask.java
+++ MorningOrganizer/src/com/pennapps/morningorganizer/GetInfoTask.java
@@ -32,14 +32,10 @@ public class GetInfoTask extends AsyncTask<Context, Void, String> {
 		Calendar calendar = new Calendar();
 		String calendarData = calendar.getCalendarStuff(thisContext);
 		
-		JavaMail jm = new JavaMail();
-		String emailData = jm.jm2();
-		
-		SMSCount smsData = new SMSCount();
-		String smsDataString = smsData.getSMSstring(thisContext);
+		String emailTextData = socialResponses();
 		//2. Turn values into strings, put info into 
 		//   informationString
-		informationString = weatherData + " " + smsDataString + ". " + emailData +". " + calendarData;
+		informationString = weatherData + " " + emailTextData + " " + calendarData;
 		
 		
 		return informationString;
@@ -51,4 +47,60 @@ public class GetInfoTask extends AsyncTask<Context, Void, String> {
 		
 		nuanceObject.speakTheString(result, thisContext);
 	}
+	
+	protected String socialResponses()
+	{	
+		JavaMail jm = new JavaMail();
+		int unreadEmails = jm.jm();
+		
+		SMSCount smsData = new SMSCount();
+		int unreadTexts = smsData.getSMSCount(thisContext);
+		
+		//String smsDataString = smsData.getSMSstring(thisContext);
+		
+		String noMessagesArray[] = {
+				"No new messages... what a surprise.",
+				"No new messages... Time to invest in tissues...",
+				"Good job getting no new texts or emails."
+		};
+		
+		String fewMessagesArray[] = {
+				"%d emails and %d texts. No surprises there...",		
+				"%d emails and %d texts... Bet you wish you were more popular...",
+				"You have %d emails and %d texts... you should work on being nicer so you can get more"				
+		};
+	
+		String manyMessagesArray[] = {
+				"Ooh, looks like mister popular has %d emails and %d texts today!",
+				"%d emails and %d texts. Clearly SOMEONE still cares about you.",
+				"Wow, %d emails and %d texts! That's a surprise!"
+		};
+		
+		String returnResponse = new String();
+		
+		if  ((unreadEmails + unreadTexts) < 4)
+		{
+			if (unreadEmails == 0 && unreadTexts == 0)
+			{
+				returnResponse = noMessagesArray[(int) ((Math.random() * noMessagesArray.length) % noMessagesArray.length)];
+			
+			}
+			else
+			{
+				returnResponse = String.format(fewMessagesArray[(int) ((Math.random() * fewMessagesArray.length) % fewMessagesArray.length)], unreadEmails, unreadTexts);
+				
+			}
+		}
+		
+		else
+		{
+			returnResponse = String.format(manyMessagesArray[(int) ((Math.random() * manyMessagesArray.length) % manyMessagesArray.length)], unreadEmails, unreadTexts);
+			
+		}
+		
+		return returnResponse;
+		
+		
+	}
 }
+
--- MorningOrganizer/src/com/pennapps/morningorganizer/SMSCount.java
+++ MorningOrganizer/src/com/pennapps/morningorganizer/SMSCount.java
@@ -5,18 +5,22 @@ import android.database.Cursor;
 import android.net.Uri;
 
 public class SMSCount {
-
+	
+	static int unreadMessagesCount = -1;
 	
 	public int getSMSCount (Context context) {
-		final Uri SMS_INBOX_URI = Uri.parse("content://sms/inbox");
-	    Cursor inboxCursor = context.getContentResolver().query(SMS_INBOX_URI, null, "read = 0", null, null);
-	    int unreadMessagesCount = inboxCursor.getCount();
-	    inboxCursor.close();
-	    final Uri MMS_INBOX_URI = Uri.parse("content://mms/inbox");
-	    Cursor mInboxCursor = context.getContentResolver().query(MMS_INBOX_URI, null, "read = 0", null, null);
-	    unreadMessagesCount += mInboxCursor.getCount();
-	    mInboxCursor.close();
-	    return unreadMessagesCount;
+		if (unreadMessagesCount == -1)
+		{
+			final Uri SMS_INBOX_URI = Uri.parse("content://sms/inbox");
+			Cursor inboxCursor = context.getContentResolver().query(SMS_INBOX_URI, null, "read = 0", null, null);
+			unreadMessagesCount = inboxCursor.getCount();
+			inboxCursor.close();
+			final Uri MMS_INBOX_URI = Uri.parse("content://mms/inbox");
+			Cursor mInboxCursor = context.getContentResolver().query(MMS_INBOX_URI, null, "read = 0", null, null);
+			unreadMessagesCount += mInboxCursor.getCount();
+			mInboxCursor.close();
+		}
+		return unreadMessagesCount;
 	}
 	
 	public String getSMSstring (Context context) {
